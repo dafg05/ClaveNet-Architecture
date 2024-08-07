e71306ab785eed8bc1a258b69347858bf82a0aad
dafg05 <danialefloresg@gmail.com>
2024-08-06 20:14:24 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cnarch',
+    version='0.0.1',
+    author='Daniel Flores Garcia',
+    author_email='danialefloresg@gmail.com',
+    description='ClaveNet-Architecture',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dafg05/midiutils',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "torch"
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
+)
